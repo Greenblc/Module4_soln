357de37a90f6b9950900b44d1781721f9f1acecb
Christopher <129985655+Greenblc@users.noreply.github.com>
2024-07-19 17:22:23 -0400

    Delete speakgoodbye.js

--- speakgoodbye.js
+++
@@ -1,7 +0,0 @@
-(function (window){
-  var goodbyeSpeaker = {};
-  var speakword = "Good Bye"
-  function speak(name) {
-  console.log(speakWord + " " + name);
-}
- window.goodbyeSpeaker = goodbyeSpeaker;
